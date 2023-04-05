5fb5f496e02cde01c9349e201bcca202f9ffeda6
KostinMikhail <79811272328@ya.ru>
2023-04-06 01:58:43 +0300

    progressin2

--- app/src/main/java/com/kostlin/inspiration/MainActivity.kt
+++ app/src/main/java/com/kostlin/inspiration/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private var rows: Int = 8
     private var cols: Int = 10
 
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,34 +27,66 @@ class MainActivity : AppCompatActivity() {
             tv.setBackgroundResource(R.drawable.bg)
 
             binding.tblLayout?.addView(tblRow)
-            if (r >= 2)
-                tv.text = "Участник " + (r - 1)
-                tblRow.addView(tv)
 
-            for (c in 1..cols) {
+            tblRow.addView(tv)
+            //тут мы создаём первый столбец
+            if (r >= 2) {
+                tv.text = "Участник " + (r - 1)
+            }
+
+
+            for (c in 1..cols - 2) {
                 val tv1 = TextView(this)
+                tv1.setBackgroundResource(R.drawable.bg)
                 val et = EditText(this)
                 et.setBackgroundResource(R.drawable.bg)
-                tv1.setBackgroundResource(R.drawable.bg)
 
 
-                if (c in 2 until rows && r == 1) {
-                    tv1.text = ((c - 1).toString())
+                if (r >= 2 && c < 2) {
                     tblRow.addView(tv1)
+                    tv1.text = ((r - 1).toString())
+                    //тут мы создаём цифры в следующем столбце после Участник
+                } else  if (r in 1 until rows) {
+                    tblRow.addView(tv1)
+                    tv1.text = "nothing"
                 }
+
+                if (r == 1) {
+//                    tblRow.addView(tv1)
+//                    tv1.text = c.toString()
+                    //тут мы создаём цифры в первой строке
+                }
+
+
+//                if (c <= cols - 2 && r >= 2) {
+//                    tblRow.addView(et)
+//                    тут мы делаем Эдит Вью
+//                }
+            }
+
+
+//            for (c in 1..cols) {
+//                val tv1 = TextView(this)
+//                val et = EditText(this)
+//                et.setBackgroundResource(R.drawable.bg)
+//                tv1.setBackgroundResource(R.drawable.bg)
+//
+//                if (c in 2 until rows && r == 1) {
+//                    tv1.text = ((c - 1).toString())
+//                    tblRow.addView(tv1)
+//                }
 //                if (c == 1 && r >= 2) {
 //                    tv1.text = ((r - 1).toString())
 //                    tblRow.addView(tv1)
 //                }
-                if (c <= cols - 2 && r >= 2) {
-                    tblRow.addView(et)
-                    //тут мы делаем Эдит Вью
-                }
-                //tv1.tag = "$c+$r"
-
-            }
+//                if (c <= cols - 2 && r >= 2) {
+//                    tblRow.addView(et)
+//                    //тут мы делаем Эдит Вью
+//                }
+//                //tv1.tag = "$c+$r"
+//
+//            }
         }
     }
 }
-
 
